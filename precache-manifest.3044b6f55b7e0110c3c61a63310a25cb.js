9083ef0129644a29da09adbe9cfd3c6ed98e359e
lionbitrade <guillem.lion.benosa@gmail.com>
2018-10-21 23:35:08 +0200

    Updates

---
+++ precache-manifest.3044b6f55b7e0110c3c61a63310a25cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdf920ac1930832a80aa",
+    "url": "/dappnode-dweb-template/static/js/runtime~main.cdf920ac.js"
+  },
+  {
+    "revision": "d61f38f820c44d5d90c4",
+    "url": "/dappnode-dweb-template/static/js/main.d61f38f8.chunk.js"
+  },
+  {
+    "revision": "d7b27eab6e2bc0b713d1",
+    "url": "/dappnode-dweb-template/static/js/1.d7b27eab.chunk.js"
+  },
+  {
+    "revision": "d61f38f820c44d5d90c4",
+    "url": "/dappnode-dweb-template/static/css/main.53f33b1d.chunk.css"
+  },
+  {
+    "revision": "0a4ff68835bc955eb3061b6dbe3b4bca",
+    "url": "/dappnode-dweb-template/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fdb1b7513f5cff702c6102fa44002b77.js"
+  "/dappnode-dweb-template/precache-manifest.3044b6f55b7e0110c3c61a63310a25cb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/dappnode-dweb-template/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
